215dba771981dc1a19a44d11202ab524e31f435c
xyb7910 <1502709024@qq.com>
2024-04-21 22:13:17 +0800

    vue第四次提交

---
+++ 19单文件组件/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <School></School>
+        <Student></Student>
+    </div>
+</template>
+
+<script>
+import School from './School.vue';
+import Student from './Student.vue';
+
+export default {
+    name: 'App',
+    // 汇总所有的组件
+    components: {
+        Student,
+        School,
+    }
+}
+</script>
+
+<style>
+
+</style>
